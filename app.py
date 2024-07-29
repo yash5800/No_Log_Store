d2225d8dccb87028f210b92fcabaa04d8620f5a3
A.Yaswanth Kiran <113412043+yash5800@users.noreply.github.com>
2024-07-29 20:01:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def upload_file():
 def download_file():
       file_name = request.form["file_name"]
       print("entered to download file : ", file_name)
-      file_url = f"https://raw.githubusercontent.com/{ os.getenv["user"] }/{ os.getenv["repo"] }e/master/{file_name}"
+      file_url = f"https://raw.githubusercontent.com/{ os.getenv("user") }/{ os.getenv("repo") }e/master/{file_name}"
       try:
             response = requests.get(file_url)
             response.raise_for_status()  # Ensure we notice bad responses
