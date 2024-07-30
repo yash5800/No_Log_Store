6d392102f9a4bb173ec68d4a93476e7376105678
A.Yaswanth Kiran <113412043+yash5800@users.noreply.github.com>
2024-07-30 21:17:23 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,12 @@
+const text = document.querySelector(".span1");
+const load = () => {
+             setTimeout(() =>{
+              text.textContent = "Dark Say!";
+            },0);
+            setTimeout(() =>{
+                text.textContent = "Don't Forget key!";
+              },5000);
+          
+        }
 
+        setInterval(load, 5000);
