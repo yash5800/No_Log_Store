a112966e4ae8c317a910516c65f0c52e0350a004
A.Yaswanth Kiran <113412043+yash5800@users.noreply.github.com>
2024-08-04 13:44:45 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,12 +1,24 @@
 const text = document.querySelector(".span1");
-const load = () => {
-             setTimeout(() =>{
-              text.textContent = "Dark Say's!";
-            },0);
-            setTimeout(() =>{
-                text.textContent = "Don't Forget key!";
-              },5000);
-          
-        }
 
-        setInterval(load, 5000);
+const load = () => {
+     setTimeout(() =>{
+      text.textContent = "Dark Say!";
+    },0);
+    setTimeout(() =>{
+        text.textContent = "Don't Forget key!";
+      },5000);
+  
+}
+
+setInterval(load, 5000);
+
+
+function cho(){
+  document.getElementById("file").click();
+}
+
+document.getElementById("file").addEventListener("change",() =>{
+  document.getElementById("sta").innerText = "Please wait...";
+  document.getElementById("sta").style.color = "#03bcf4";
+  document.getElementById("upload").submit();
+})
