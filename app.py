ca92422b6134b07e28724708dd9992427d12b16f
A.Yaswanth Kiran <113412043+yash5800@users.noreply.github.com>
2024-11-11 19:04:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
-from flask import Flask, render_template, request, session, send_file,send_from_directory
+from flask import Flask, render_template, request, session, send_file
 from git_repo import *
 from sql_operations import *
 from io import BytesIO
 import mimetypes
 import os
+import time
 import requests
 import secrets
-import aiohttp
-import asyncio
-
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
@@ -17,12 +15,6 @@ app.secret_key = secrets.token_hex(16)
 @app.route('/')
 def main():
     return render_template('key.html')
-
-
-@app.route('/ads.txt')
-def ads_txt():
-    return send_from_directory('static', 'ads.txt')
-
 
 @app.route('/check_shit', methods=['POST', 'GET'])
 def check_shit():
@@ -70,54 +62,27 @@ def upload_file():
         else:
             return render_template('index.html', status='Not uploaded/File Name Already exists', user=session['username'], data=retrive(session['username']))
 
-
-async def download_file_async(file_name):
+@app.route('/download', methods=['GET', 'POST'])
+def download_file():
+    file_name = request.form["file_name"]
+    print("entered to download file:", file_name)
     file_url = f"https://raw.githubusercontent.com/yash5800/ND_store/master/{file_name}"
     try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(file_url) as response:
-                if response.status == 200:
-                    # Read content of the file
-                    content = await response.read()
-
-                    # Determine MIME type
-                    filename = os.path.basename(file_url)
-                    mime_type, _ = mimetypes.guess_type(filename)
-                    if mime_type is None:
-                        mime_type = 'application/octet-stream'
-
-                    # Return file content as BytesIO
-                    return BytesIO(content), mime_type, filename
-                else:
-                    return None, None, None
-    except Exception as e:
-        print(f"Error downloading {file_name}: {e}")
-        return None, None, None
-
-# Async route for downloading files
-@app.route('/download', methods=['GET', 'POST'])
-async def download_files():
-    if request.method == 'POST':
-        file_name = request.form.get("file_name")
-        if not file_name:
-            return render_template('index.html', status='No file name provided', user=session.get('username'), data=retrive(session.get('username')))
+        response = requests.get(file_url)
+        response.raise_for_status()
+            
+        filename = os.path.basename(file_url)
+        mime_type, _ = mimetypes.guess_type(filename)
+        if mime_type is None:
+            mime_type = 'application/octet-stream'
+            
+        file_content = BytesIO(response.content)
+        return send_file(file_content, mimetype=mime_type, as_attachment=True, download_name=filename)
         
-        print("Entered to download file:", file_name)
-        
-        # Call the async function to download the file
-        file_content, mime_type, filename = await download_file_async(file_name)
-        
-        if file_content:
-            return send_file(file_content, mimetype=mime_type, as_attachment=True, download_name=filename)
-        else:
-            return render_template('index.html', status=f"Error downloading {file_name}", user=session.get('username'), data=retrive(session.get('username')))
+    except requests.RequestException as e:
+        print(f"Error fetching file: {e}")
+        return render_template('index.html', status='Unable to Download', user=session['username'], data=retrive(session['username']))
     
-    return render_template('index.html', user=session.get('username'), data=retrive(session.get('username')))
-
-
-
-    
-
 @app.route('/view', methods=['GET', 'POST'])
 def view():
     file_name = request.form["file_name"]
@@ -138,13 +103,11 @@ def view():
 def display_pdf(pdf_path):
     return send_file(pdf_path, as_attachment=False)
 
-
 @app.route('/fuck_off', methods=['GET', 'POST'])
 def fuck_off():
     file_name = request.form["file_name"]
+    print("Deleting file" + file_name)
     result = delete_file(session['username'], file_name)
-    
-    
     if result:
         
         result = delete_from_github(file_name)
